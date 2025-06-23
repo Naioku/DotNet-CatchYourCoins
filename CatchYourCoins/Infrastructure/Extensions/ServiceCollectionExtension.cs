6f727c3803cc4534d07287cc509cf2c204951231
Naioku <adrian.komuda.dev@gmail.com>
2025-06-23 19:56:14 +0200

    Added EntityFramework.

---
+++ CatchYourCoins/Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using Domain.IdentityEntities;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(
+            option => option
+                .UseSqlServer(configuration.GetConnectionString("Main")));
+
+        // services
+        //     .AddIdentityCore<AppUser>()
+        //     .AddRoles<AppRole>()
+        //     .AddEntityFrameworkStores<AppDbContext>()
+        //     .AddDefaultTokenProviders()
+        //     .AddUserStore<UserStore<AppUser, AppRole, AppDbContext, Guid>>()
+        //     .AddRoleStore<RoleStore<AppRole, AppDbContext, Guid>>();
+        
+        // services.AddScoped<>();
+        
+        return services;
+    }
+}
---
+++ CatchYourCoins/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
