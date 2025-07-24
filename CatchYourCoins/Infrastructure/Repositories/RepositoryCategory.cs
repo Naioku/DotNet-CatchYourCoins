61e11fead4648e97a1c2266b7c8bb403656a0a15
Naioku <adrian.komuda.dev@gmail.com>
2025-07-24 11:55:35 +0200

    Bugfix: RepositoryCategory: GetCategoryById not protected from unauthorized access.

--- CatchYourCoins/Infrastructure/Repositories/RepositoryCategory.cs
+++ CatchYourCoins/Infrastructure/Repositories/RepositoryCategory.cs
@@ -1,12 +1,20 @@
 ﻿using Domain.Dashboard.Entities;
 using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Infrastructure.Extensions;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
-public class RepositoryCategory(AppDbContext dbContext) : IRepositoryCategory
+public class RepositoryCategory(
+    AppDbContext dbContext,
+    IServiceCurrentUser serviceCurrentUser) : IRepositoryCategory
 {
     public async Task CreateCategoryAsync(Category category) => await dbContext.Categories.AddAsync(category);
-    public async Task<Category?> GetCategoryByIdAsync(int id) => await dbContext.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+    public async Task<Category?> GetCategoryByIdAsync(int id) =>
+        await dbContext.Categories
+            .WhereAuthorized(serviceCurrentUser.User.Id)
+            .FirstOrDefaultAsync(c => c.Id == id);
 }
