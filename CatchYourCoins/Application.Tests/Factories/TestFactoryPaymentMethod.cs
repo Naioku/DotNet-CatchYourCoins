a2bbfb6e23db5a1747277d4d5941cf781b28260a
Naioku <adrian.komuda.dev@gmail.com>
2025-07-25 17:13:59 +0200

    Added query: GetAllPaymentMethods.

--- CatchYourCoins/Application.Tests/Factories/TestFactoryPaymentMethod.cs
+++ CatchYourCoins/Application.Tests/Factories/TestFactoryPaymentMethod.cs
@@ -1,14 +1,26 @@
-﻿using Domain.Dashboard.Entities;
+﻿using System.Collections.Generic;
+using Domain.Dashboard.Entities;
 
 namespace Application.Tests.Factories;
 
 public class TestFactoryPaymentMethod
 {
-    public static PaymentMethod CreatePaymentMethod(CurrentUser currentUser) => new()
+    public static PaymentMethod CreatePaymentMethod(CurrentUser currentUser, int id = 1) => new()
     {
         Id = 1,
         Limit = 100,
         Name = "Test",
         UserId = currentUser.Id
     };
+
+    public static List<PaymentMethod> CreatePaymentMethods(CurrentUser currentUser, int quantity)
+    {
+        List<PaymentMethod> result = [];
+        for (int i = 0; i < quantity; i++)
+        {
+            result.Add(CreatePaymentMethod(currentUser, i + 1));
+        }
+        
+        return result;
+    }
 }
