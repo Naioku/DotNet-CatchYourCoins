2a95fcc59c4f40ec00acc00d7000c80e2a7952a7
Naioku <adrian.komuda.dev@gmail.com>
2025-07-28 11:10:12 +0200

    Added query: GetIncomeById.

---
+++ CatchYourCoins/Application.Tests/Incomes/Queries/GetById/HandlerGetIncomeByIdTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Application.DTOs.Incomes;
+using Application.Incomes.Queries.GetById;
+using Application.Tests.Factories;
+using Domain.Dashboard.Entities;
+using Domain.Interfaces.Repositories;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Application.Tests.Incomes.Queries.GetById;
+
+[TestSubject(typeof(HandlerGetIncomeById))]
+public class HandlerGetIncomeByIdTest
+    : TestHandlerGetById<
+        HandlerGetIncomeById,
+        Income,
+        IncomeDTO,
+        QueryGetIncomeById,
+        IRepositoryIncome,
+        TestFactoryIncome
+    >
+{
+    protected override HandlerGetIncomeById CreateHandler() =>
+        new(GetMock<IRepositoryIncome>().Object);
+    
+    protected override QueryGetIncomeById GetQuery() => new() { Id = 1 };
+
+    [Fact]
+    public async Task GetOne_ValidData_ReturnedOne()
+    {
+        await GetOne_ValidData_ReturnedOne_Base((inputEntity, resultDTO) =>
+        {
+            Assert.Equal(inputEntity.Id, resultDTO.Id);
+            Assert.Equal(inputEntity.Amount, resultDTO.Amount);
+            Assert.Equal(inputEntity.Date, resultDTO.Date);
+            Assert.Equal(inputEntity.Description, resultDTO.Description);
+            Assert.NotNull(inputEntity.Category);
+            Assert.Equal(inputEntity.Category.Name, resultDTO.Category);
+        });
+    }
+
+    [Fact]
+    public async Task GetOne_NoEntryAtPassedID_ReturnedNull() =>
+        await GetOne_NoEntryAtPassedID_ReturnedNull_Base();
+}
---
+++ CatchYourCoins/Application/Incomes/Queries/GetById/GetIncomeById.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs.Incomes;
+using Application.Requests.Queries.GetById;
+using Domain.Dashboard.Entities;
+using Domain.Interfaces.Repositories;
+
+namespace Application.Incomes.Queries.GetById;
+
+public class QueryGetIncomeById : QueryGetByIdBase<IncomeDTO>;
+
+public class HandlerGetIncomeById(IRepositoryIncome repository)
+    : HandlerCRUDGetById<Income, QueryGetIncomeById, IncomeDTO>(repository)
+{
+    protected override Dictionary<string, string> GetFailureMessages() =>
+        new()
+        {
+            { "Income", "Income not found" }
+        };
+
+    protected override IncomeDTO MapEntityToDTO(Income entity) =>
+        new()
+        {
+            Id = entity.Id,
+            Amount = entity.Amount,
+            Date = entity.Date,
+            Description = entity.Description,
+            Category = entity.Category?.Name,
+        };
+}
