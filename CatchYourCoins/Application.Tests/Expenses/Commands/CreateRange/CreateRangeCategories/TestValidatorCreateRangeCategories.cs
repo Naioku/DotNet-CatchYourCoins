207bfbd9d8215ad89d8eda98aae76b0f132c2de2
Naioku <adrian.komuda.dev@gmail.com>
2025-07-30 13:00:39 +0200

    Added command: CreateRangeCategories.

---
+++ CatchYourCoins/Application.Tests/Expenses/Commands/CreateRange/CreateRangeCategories/TestValidatorCreateRangeCategories.cs
@@ -0,0 +1,64 @@
+﻿using Application.DTOs.InputDTOs.Expenses;
+using Application.Expenses.Commands.CreateRange;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Application.Tests.Expenses.Commands.CreateRange.CreateRangeCategories;
+
+[TestSubject(typeof(ValidatorCreateRangeCategories))]
+public class TestValidatorCreateRangeCategories
+    : TestValidatorBase<ValidatorCreateRangeCategories, CommandCreateRangeCategories>
+{
+    [Fact]
+    public void Validate_AllValidData_NoError() =>
+        AssertSuccess(new CommandCreateRangeCategories
+        {
+            Data = [
+                new InputDTOExpenseCategory
+                {
+                    Name = "Test1",
+                    Limit = 1000
+                },
+                new InputDTOExpenseCategory
+                {
+                    Name = "Test2",
+                    Limit = 2000
+                },
+            ],
+            
+        });
+
+    [Fact]
+    public void Validate_MinimalValidData_NoError() =>
+        AssertSuccess(new CommandCreateRangeCategories
+        {
+            Data = [
+                new InputDTOExpenseCategory
+                {
+                    Name = "Test1",
+                },
+                new InputDTOExpenseCategory
+                {
+                    Name = "Test2",
+                },
+            ],
+        });
+
+    [Fact]
+    public void Validate_EmptyName_Error() =>
+        AssertFailure(new CommandCreateRangeCategories
+        {
+            Data = [
+                new InputDTOExpenseCategory
+                {
+                    Name = "",
+                    Limit = 1000
+                },
+                new InputDTOExpenseCategory
+                {
+                    Name = "",
+                    Limit = 2000
+                },
+            ],
+        });
+}
