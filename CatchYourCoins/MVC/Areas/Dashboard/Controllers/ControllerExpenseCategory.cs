87df3eb21674611cf7d8dcce445222e20780f45e
Naioku <adrian.komuda.dev@gmail.com>
2025-08-18 11:16:23 +0200

    Added: ExpenseCategories: Index.

--- CatchYourCoins/MVC/Areas/Dashboard/Controllers/ControllerExpenseCategory.cs
+++ CatchYourCoins/MVC/Areas/Dashboard/Controllers/ControllerExpenseCategory.cs
@@ -1,7 +1,11 @@
 ﻿using Application.Dashboard.Commands;
 using Application.Dashboard.DTOs;
 using Application.Dashboard.DTOs.CreateDTOs.Expenses;
+using Application.Dashboard.DTOs.OutputDTOs.Expenses;
+using Application.Dashboard.Queries;
 using Domain;
+using Domain.Dashboard.Entities.Expenses;
+using Domain.Dashboard.Specifications.Expenses;
 using FluentValidation;
 using FluentValidation.Results;
 using MediatR;
@@ -11,7 +15,7 @@ using MVC.Extensions;
 namespace MVC.Areas.Dashboard.Controllers;
 
 [Area("Dashboard")]
-public class ExpenseCategory(
+public class ControllerExpenseCategory(
     IMediator mediator,
     IValidator<DTORange<CreateDTOExpenseCategory>> validatorCreateExpenseCategory)
     : Controller
@@ -22,26 +26,26 @@ public class ExpenseCategory(
     public async Task<IActionResult> Create(DTORange<CreateDTOExpenseCategory> dtos, CancellationToken cancellationToken)
     {
         ValidationResult validationResult = await validatorCreateExpenseCategory.ValidateAsync(dtos, cancellationToken);
-        
+
         if (!validationResult.IsValid)
         {
             validationResult.AddToModelState(ModelState);
             return View(dtos);
         }
-        
+
         CommandCRUDCreateRange<CreateDTOExpenseCategory> command = new()
         {
             Data = dtos.Items,
         };
-        
+
         Result result = await mediator.Send(
             command,
             cancellationToken
         );
-        
+
         if (result.IsSuccess)
         {
-            return View();
+            return RedirectToAction("Index", "ControllerExpenseCategory");
         }
 
         foreach (KeyValuePair<string, string> error in result.Errors)
@@ -50,5 +54,30 @@ public class ExpenseCategory(
         }
 
         return View(dtos);
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        Result<IReadOnlyList<OutputDTOExpenseCategory>> dto = await mediator.Send(
+            new QueryCRUDGet<ExpenseCategory, OutputDTOExpenseCategory>
+            {
+                Specification = SpecificationExpenseCategory.GetBuilder().Build(),
+            }
+        );
+
+        if (!dto.IsSuccess)
+        {
+            foreach (KeyValuePair<string, string> error in dto.Errors)
+            {
+                ModelState.AddModelError(error.Key, error.Value);
+            }
+
+            return View();
+        }
+
+        return View(new DTORange<OutputDTOExpenseCategory>
+        {
+            Items = dto.Value.ToList(),
+        });
     }
 }
